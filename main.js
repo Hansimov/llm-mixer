19cc8e6734e6e0bf461b00b00361ab5bb5b42ad1
Hansimov <591172499@qq.com>
2023-12-16 00:08:20 +0800

    :recycle: [Refactor] Remove live.js and commons_loader.js

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 import { ButtonsBinder } from "./components/buttons_binder.js";
 import { InputsBinder } from "./components/inputs_binder.js";
-import { CommonsLoader } from "./commons/commons_loader.js";
 
 $(document).ready(function () {
     let buttons_binder = new ButtonsBinder();
     buttons_binder.bind();
     let inputs_binder = new InputsBinder();
     inputs_binder.bind();
-    let commons_loader = new CommonsLoader();
-    commons_loader.load();
 });
