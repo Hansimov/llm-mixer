c00fa7c6fc739592758f79e713e6594a3216e5b1
Hansimov <591172499@qq.com>
2024-01-16 15:49:42 +0800

    :gem: [Feature] Responsive and adaptive chat-history-sidebar

--- components/inputs_binder.js
+++ components/inputs_binder.js
@@ -9,6 +9,9 @@ export class InputsBinder {
         let chat_session_container_resize_binder =
             new ChatSessionContainerResizeBinder();
         chat_session_container_resize_binder.bind();
+        let chat_history_sidebar_resize_binder =
+            new ChatHistorySidebarResizeBinder();
+        chat_history_sidebar_resize_binder.bind();
     }
 }
 
@@ -31,7 +34,7 @@ class ChatSessionContainerResizeBinder {
     constructor() { }
     bind() {
         this.resize();
-        $(window).on("resize", this.resize);
+        $(window).resize(this.resize.bind(this));
     }
     resize() {
         let user_interaction_height = $("#user-interactions").outerHeight(true);
@@ -40,5 +43,51 @@ class ChatSessionContainerResizeBinder {
             "max-height",
             page_height - user_interaction_height + "px"
         );
+    }
+}
+
+class ChatHistorySidebarResizeBinder {
+    constructor() {
+        this.USER_INTERACTIONS_MAX_WIDTH = 900;
+        this.SIDEBAR_GAP = 10;
+        this.SIDEBAR_MAX_WIDTH = 300;
+        this.SIDEBAR_MIN_WIDTH = 120;
+    }
+    bind() {
+        this.resize();
+        $(window).resize(this.resize.bind(this));
+    }
+    get_window_width() {
+        return $(window).width();
+    }
+    get_user_interations_width() {
+        return $("#user-interactions").width();
+    }
+    get_side_margin() {
+        return (this.get_window_width() - this.get_user_interations_width()) / 2 - this.SIDEBAR_GAP;
+    }
+    resize() {
+        let sidebar = $("#chat-history-sidebar");
+        let user_interactions = $("#user-interactions");
+        let is_sidebar_show = sidebar[0].classList.contains("show");
+        if (this.get_side_margin() >= this.SIDEBAR_MAX_WIDTH) {
+            if (!is_sidebar_show) {
+                sidebar.addClass("show");
+            }
+            sidebar.css("max-width", this.SIDEBAR_MAX_WIDTH + "px");
+            sidebar.css("min-width", this.SIDEBAR_MIN_WIDTH + "px");
+        } else if (this.get_side_margin() <= this.SIDEBAR_MIN_WIDTH) {
+            if (is_sidebar_show) {
+                sidebar.removeClass("show");
+            }
+            sidebar.css("max-width", this.SIDEBAR_MAX_WIDTH + "px");
+            sidebar.css("min-width", this.SIDEBAR_MIN_WIDTH + "px");
+        } else {
+            if (!is_sidebar_show) {
+                sidebar.addClass("show");
+            }
+            sidebar.css("max-width", this.get_side_margin());
+            sidebar.css("min-width", this.SIDEBAR_MIN_WIDTH + "px");
+        }
     }
 }
